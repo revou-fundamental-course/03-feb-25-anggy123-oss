35f42820e170be61e5c7c593e661f84e1d417f46
ANGGYANA FAITH LUMBANTOBING <anggyanafaithlumbantobing@gmail.com>
2025-02-13 14:21:44 +0700

    update javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 // Ini file Javascript
+
+console.log('Javascript is working!');
+
+function validateForm() {
+  let input = document.getElementById('main-input');
+  console.log(input.value);
+
+  if (input.value == '') {
+    alert('Tolong isi inputan');
+  } else {
+    /// Proses konversi
+    let calc = convertToCelcius(input.value)
+     
+    /// Tampilkan hasil
+    document.getElementById('main-result').value = calc;
+    document.getElementById('cara-konversi').value = input.value + ' * 9/5 + 32 = ' + calc ;
+    console.log ('Berhasil di eksekusi');
+  }
+}
+
+/// Fungsi konversi
+let convertToCelcius = (input) => {
+  return input * 1.8 + 32;
+}
